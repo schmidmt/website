0bb7b2433250e231e61388a8d1d482925b3e7e79
Michael T. Schmidt <schmidmt@gmail.com>
2014-04-06 11:41:56 -0500

    Updated skills

--- js/script.js
+++ js/script.js
@@ -5,8 +5,15 @@ $(document).ready(function() {
     applyNavigation();
     applyNavHeightFix();
     applyResize();
-    //decodeEmail();
-    applySkills();
+
+    $("img.lazy").lazyload({
+      threshold : 200
+    });
+
+    $("div.lazy").lazyload({
+      effect : "fadeIn"
+    });
+
 })
 
 
@@ -41,12 +48,7 @@ function applyScrollSpy() {
 }
 
 function applyStickyNav() {
-    //stickyNavDisp = $('.scroll-down').offset().top + $('.scroll-down').outerHeight();
-
     stickyNavDisp = $(window).height()
-
-    //console.log('Applying StickyNav. offset = ' + stickyNavDisp )
-
     $(window).on('scroll', function() {
         stickyNav();
     });
@@ -55,10 +57,8 @@ function applyStickyNav() {
 
 function stickyNav() {
     if ($(window).scrollTop() > stickyNavDisp) {
-        //console.log("Adding Fixed to nav")
         $('body').addClass('fixed');
     } else {
-        //console.log("Removing Fixed to nav")
         $('body').removeClass('fixed');
     }
 }
@@ -71,47 +71,8 @@ function applyNavHeightFix() {
 
 function applyResize() {
     $(window).on('resize', function() {
-        //console.log('Applying StickyNav. offset = ' + $('.scroll-down').offset().top + $('.scroll-down').outerHeight() )
         stickyNavDisp = $('.scroll-down').offset().top + $('.scroll-down').outerHeight();
-        /*$('.jumbotron').css({
-            height: ($(window).height()) + 'px'
-        });*/
         applyHeader();
         applyNavHeightFix();
     });
-}
-
-function decodeEmail() {
-    $('a[href*=mailto]').on('click', function(e) {
-        var lstrEmail = $(this).attr('href').replace('mailto:', '');
-        //console.log('found email: ' + lstrEmail)
-        lstrEmail = rot13(lstrEmail)
-        //console.log('replacing with: ' + lstrEmail)
-        $(this).attr('href', 'mailto:' + lstrEmail);
-    });
-}
-
-function rot13(str) {
-    return str.replace(/([a-zA-Z])/g,
-        function(m) {
-            var num = m.toUpperCase().charCodeAt(0) + 13;
-            var ch = (num > 90) ? 64 + (num - 90) : num;
-            var character = String.fromCharCode(ch);
-            return (m == m.toUpperCase()) ? character : character.toLowerCase();
-        });
-}
-
-function applySkills() {
-  /*
-  var symbol = "glyphicon glyphicon-cog";
-  //$('.ability-value').
-  //
-  console.log("hit applySkills");
-  console.log("ability-value elements:")
-  var abilityValues = document.getElementsByClassName("ability-value")
-  console.log("Found " + abilityValues.length + " objects")
-  for ( i = 0 ; i < abilityValues.length ; i++) {
-    console.log(i + " " + abilityValues[i].parentNode.firstChild.innerHTML + " -> Classes: " + abilityValues[i].classList);
-  }
-  */
 }
